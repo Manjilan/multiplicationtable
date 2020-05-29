9fb6db4bb66c502d0a7b5e2c8eb3bc480ec3ef0b
Manjilan <nakarmimanjila@yahoo.com>
2020-05-29 14:57:45 -0700

    initialized repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Input } from 'antd';
 import './App.css';
 
 function App() {
+  let [table,setTable] = useState();
+  let calculateTable= grid=>{
+    let table = [];
+    for(let i=1;i<=grid;i++){
+      let row = [];
+      for(let j=1;j<=grid;j++){
+        row.push(i*j);
+      }
+      table.push(row);
+    }
+    return table;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Enter the multiplication table</h3>
+        <Input placeholder="Enter the multiplication table" onChange={(e)=>setTable(calculateTable(e.target.value))}/>
+        <div>
+        {table? table.map((row, j)=>{
+          let element = row.map((el, i)=>{
+            return <div className = "grid" key={i}>{el}</div>;
+          })
+          return <div className = "row" key={j}>{element}</div>;
+        }) : <div></div>}
+        </div>
       </header>
     </div>
   );
